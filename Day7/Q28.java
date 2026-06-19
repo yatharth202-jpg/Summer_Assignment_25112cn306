30ec4b3ba5ba0a56e1b36e035d75843df03f285d
yatharth202-jpg <206231322+yatharth202-jpg@users.noreply.github.com.>
2026-06-19 21:31:35 +0530

    update

--- Day7/Q28.java
+++ Day7/Q28.java
@@ -1,12 +1,31 @@
 //WAP to recursive reverse a number
-import java.util.*;
+import java.util.Scanner;
 
 public class Q28 {
-    public static void main(String args[]) {
+    
+    public static int reverseRecursive(int num, int reversed) {
+         if (num == 0) {
+            return reversed;
+        }
+        
+        int lastDigit = num % 10;
+                
+        int updatedReversed = (reversed * 10) + lastDigit;
+        
+        return reverseRecursive(num / 10, updatedReversed);
+    }
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number : ");
-        int n = sc.nextInt();
-
-     }
+        
+        System.out.print("Enter an integer to reverse: ");
+        int number = sc.nextInt();
+        
+        int reversedNumber = reverseRecursive(number, 0);
+        
+        System.out.println("Original Number: " + number);
+        System.out.println("Reversed Number: " + reversedNumber);
+        
+        sc.close();
+    }
 }
