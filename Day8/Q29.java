0bee8c116365ea8467131ca35bae70e38a6e466e
yatharth202-jpg <206231322+yatharth202-jpg@users.noreply.github.com.>
2026-06-20 23:53:11 +0530

    Q30 Added

---
+++ Day8/Q29.java
@@ -0,0 +1,20 @@
+//WAP to print half pyramid pattern
+import java.util.*;
+
+public class Q29 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        //input
+      System.out.print("Enter number of rows : ");
+        int n = sc.nextInt();
+
+        //output
+      for(int i=1; i<=n; i++) {
+          for(int j=1; j<=i; j++) {
+              System.out.print("* ");
+          }
+          System.out.println();
+      }
+    }
+}
