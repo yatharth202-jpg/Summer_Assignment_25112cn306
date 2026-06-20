617d1824c74d6bb48a7a3ec8e9fe8c57258c2afa
yatharth202-jpg <206231322+yatharth202-jpg@users.noreply.github.com.>
2026-06-21 00:03:19 +0530

    Q32 updated

--- Day8/Q32.java
+++ Day8/Q32.java
@@ -1,3 +1,23 @@
+/* WAP to print repeated number
+   1
+   2 2
+   3 3 3
+   4 4 4 4
+   5 5 5 5 5
+*/
+import java.util.*;
+
 public class Q32 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
 }
