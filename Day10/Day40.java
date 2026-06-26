3e18240b85c88d5e0eee4281cbf128794f517040
yatharth202-jpg <206231322+yatharth202-jpg@users.noreply.github.com.>
2026-06-26 17:53:24 +0530

    update

---
+++ Day10/Day40.java
@@ -0,0 +1,39 @@
+package Assignment.Day10;
+/*
+WAP to print character triangle pattern
+       A
+     A B A  
+   A B C B A
+ A B C D C B A
+ */
+import java.util.Scanner;
+
+public class Day40 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            // Print leading spaces
+            for (int j = rows; j > i; j--) {
+                System.out.print("  ");
+            }
+
+            // Print increasing characters
+            for (char ch = 'A'; ch < 'A' + i; ch++) {
+                System.out.print(ch + " ");
+            }
+
+            // Print decreasing characters
+            for (char ch = (char) ('A' + i - 2); ch >= 'A'; ch--) {
+                System.out.print(ch + " ");
+            }
+
+            System.out.println();
+        }
+         sc.close();
+ }
+}
+
