978817d39eee737066ed05bff62a1c1b7e4ed104
yatharth202-jpg <206231322+yatharth202-jpg@users.noreply.github.com.>
2026-06-26 17:23:53 +0530

    update

---
+++ Day9/Q36.java
@@ -0,0 +1,31 @@
+package Assignment.Day9;
+/*
+WAP to print pattern 
+  *****
+  *   *
+  *   *
+  *   *
+  ***** 
+ */
+
+import java.util.Scanner;
+public class Q36 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.print("Enter the number of rows: ");
+    int rows = sc.nextInt();
+
+    for (int i = 1; i <= rows; i++) {
+      for (int j = 1; j <= rows; j++) {
+        if (i == 1 || i == rows || j == 1 || j == rows) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+  }
+    
+}
