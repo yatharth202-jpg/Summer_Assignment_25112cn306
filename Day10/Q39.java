df2a4b0214ea1aabc544bcf4d12c65084e2e9649
yatharth202-jpg <206231322+yatharth202-jpg@users.noreply.github.com.>
2026-06-26 17:44:22 +0530

    update

--- Day10/Q39.java
+++ Day10/Q39.java
@@ -1,5 +1,11 @@
 package Assignment.Day10;
-
+/*
+WAP to print pattern
+      1
+    1 2 1
+  1 2 3 2 1
+1 2 3 4 3 2 1
+*/
 import java.util.Scanner;
 public class Q39 {
     public static void main(String[] args) {
@@ -7,7 +13,18 @@ public class Q39 {
 
         System.out.print("Enter the number of rows: ");
         int rows = sc.nextInt();
-        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k + " ");
+            }
+            for (int l = i - 1; l >= 1; l--) {
+                System.out.print(l + " ");
+            }
+            System.out.println();
+        }
 
     sc.close();
  }
