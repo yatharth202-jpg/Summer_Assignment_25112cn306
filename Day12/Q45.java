47e974b969293f061fef317d8581f36c7c4c7813
yatharth202-jpg <206231322+yatharth202-jpg@users.noreply.github.com.>
2026-06-27 19:43:09 +0530

    update

---
+++ Day12/Q45.java
@@ -0,0 +1,38 @@
+package Assignment.Day12;
+
+//WAP to write function for palendrome
+import java.util.Scanner;
+
+public class Q45 {
+
+    public static boolean IsPalendrome(int num) {
+        int OriginalNum = num;
+        int result = 0;
+
+        while(num>0) {
+    
+        int digit = num % 10 ;
+
+        result = result*10 + digit;
+
+        num = num/10;
+        }
+
+        if(result == OriginalNum) {
+            System.out.println("This number is a palendrome");
+            return true;
+        } else {
+            System.out.println("This number is a palendrome");
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+
+        IsPalendrome(num);
+        sc.close();
+    }
+}
