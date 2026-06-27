88748bae161008dd9ba2426917dd75c187e916e0
yatharth202-jpg <206231322+yatharth202-jpg@users.noreply.github.com.>
2026-06-27 17:04:02 +0530

    update

--- Day11/Q41.java
+++ Day11/Q41.java
@@ -1,5 +1,26 @@
 package Assignment.Day11;
 
+//WAP to write function to find sum of two numbers
+import java.util.Scanner;
+
 public class Q41 {
-    
+
+    public static int AddTwofNumber(int a,int b,int sum) {
+        return  sum = a+b;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter 1st number : ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter 1st number : ");
+        int b = sc.nextInt();
+        
+        int sum = AddTwofNumber(a, b,0);
+
+        System.out.print("the sum of " + a + " and " +b+ " is : " + sum);
+      
+        sc.close();
+    }
 }
--- Day11/Q43.java
+++ Day11/Q43.java
@@ -1,5 +1,14 @@
 package Assignment.Day11;
 
+//WAP to write function to find sum of two numbers
+import java.util.Scanner;
+
 public class Q43 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        sc.close();
+    }
 }
