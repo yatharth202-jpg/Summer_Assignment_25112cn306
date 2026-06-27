019bb0cfaff1bd81caa06326388262cc9e6ad8f2
yatharth202-jpg <206231322+yatharth202-jpg@users.noreply.github.com.>
2026-06-27 17:10:02 +0530

    update

--- Day11/Q42.java
+++ Day11/Q42.java
@@ -1,14 +1,29 @@
 package Assignment.Day11;
 
-//WAP to write function to find sum of two numbers
+//WAP to write function to find maximum
 import java.util.Scanner;
 
 public class Q42 {
+    public static int FindMaximum(int a,int b){
+        if(a>b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Enter a number : ");
-        int n = sc.nextInt();
+        System.out.print("Enter 1st number : ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter 2nd number : ");
+        int b = sc.nextInt();
+
+        int c = FindMaximum(a, b);
+
+        System.out.print("The maximum number is : " + c);
+     
         sc.close();
     }
 }
