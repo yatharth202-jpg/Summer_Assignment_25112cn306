7abd82ee9c4cae373578b3b08fca5f4004d8b0b6
yatharth202-jpg <206231322+yatharth202-jpg@users.noreply.github.com.>
2026-06-27 17:33:14 +0530

    update

--- Day11/Q44.java
+++ Day11/Q44.java
@@ -1,14 +1,25 @@
 package Assignment.Day11;
 
-//WAP to write function to find sum of two numbers
+//WAP to write function to find factorial
 import java.util.Scanner;
 
 public class Q44 {
+
+    public static int FindFactorial(int n) {
+        if(n <=1) {
+            return 1;
+        }
+
+        int result = FindFactorial(n-1) * n;
+        return result;
+        }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         System.out.print("Enter a number : ");
         int n = sc.nextInt();
+
+        System.out.print("Factorial of "+ n + " is : " + FindFactorial(n) );
         sc.close();
     }
 }
