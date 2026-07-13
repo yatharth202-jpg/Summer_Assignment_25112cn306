a5f55f74f314b96d6c47afad952e6f0ea009822e
yatharth202-jpg <206231322+yatharth202-jpg@users.noreply.github.com.>
2026-07-13 15:58:50 +0530

    Q56 added

--- Day14/Q56.java
+++ Day14/Q56.java
@@ -1,27 +1,44 @@
-public package Assignment.Day14;
+package Assignment.Day14;
 
 //WAP to find dublicate
-import java.util.Scanner;
-public interface Q56 {
+import java.util.*;
 
-      public static void FindDublicates {
+public class Q56 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
-      }
-      public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int size = scanner.nextInt();
         
-        System.out.print("Enter the size of array : ");
-        int num = sc.nextInt();
-        int arr[] = new int[num];
-        int dub[] = new int[num];
-
-        for(int i = 0; i<num ; i++) {
-            System.out.print("Enter " + (i+1) + " of the array : ");
-            arr[i] = sc.nextInt();
+        int[] numbers = new int[size];
+        
+        System.out.println("Enter " + size + " integers:");
+        for (int i = 0; i < size; i++) {
+            numbers[i] = scanner.nextInt();
         }
- 
-       
-     
-        sc.close();
+        
+        Arrays.sort(numbers);
+        
+        System.out.print("Duplicate elements: ");
+        boolean foundDuplicate = false;
+        
+        for (int i = 0; i < numbers.length - 1; i++) {
+  
+            if (numbers[i] == numbers[i + 1]) {
+                System.out.print(numbers[i] + " ");
+                foundDuplicate = true;
+                
+                while (i < numbers.length - 1 && numbers[i] == numbers[i + 1]) {
+                    i++;
+                }
+            }
+        }
+        
+        if (!foundDuplicate) {
+            System.out.print("None");
+        }
+        System.out.println();
+        
+        scanner.close();
     }
 }
